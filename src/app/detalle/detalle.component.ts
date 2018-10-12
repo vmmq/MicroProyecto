e75cbb765a68b77cf32d47bc792dec77d8859cf7
System Administrator <root@Victors-MacBook-Pro.local>
2018-10-12 18:08:32 -0400

    id pasado

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+constructor(private route: ActivatedRoute) {
+    this.id = this.route.snapshot.params.id;
+}
 
   ngOnInit() {
   }
