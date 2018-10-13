6f32962a1c5d9872197f2ff357d852283d6bc63f
System Administrator <root@Victors-MacBook-Pro.local>
2018-10-12 20:26:20 -0400

    evoluciones agregadas

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -11,11 +11,29 @@ export class DetalleComponent implements OnInit {
 
   id: number;
   pokemon: Array<any>;
+  pokemones: Array<any>;
+
 
 constructor(private route: ActivatedRoute, private _servicio:PokeService) {
     this.id = this.route.snapshot.params.id;
     this.pokemon= _servicio.obtenerpokemones(this.id);
+    this.pokemones= _servicio.obtenerpokemones();
 }
+
+
+
+getEvolucion(name?: string){
+	for (let poke of this.pokemones) {
+
+        if(poke.name == name){
+       
+          return poke;
+        }
+     }     
+
+}
+
+
 
   ngOnInit() {
   }
